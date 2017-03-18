79d60509f48fb893bcaeef26945c0a3f68c96b0d
jonasde <jonasd90@gmail.com>
2017-03-18 12:38:34 +0100

    Refactor to a Minimalistic Version

--- linker.py
+++ linker.py
@@ -29,9 +29,9 @@ global force
 
 
 
-
+# ======= Assertion Handling =========
 def assert_feature(data, name):
-    if not data:
+    if data == None:
         raise ImportWarning()
     notify('processing '+name+'...')
 
@@ -50,23 +50,7 @@ def assert_path(src, dest):
         return False
     return True
 
-
-def install(**kwargs):
-
-    cache = apt.Cache()
-    js = kwargs['profile']
-    dependencies  = js.get("apt_get_dependencies")
-    
-    for pac_name in dependencies:
-    #cache = apt.Cache()
-        if cache.has_key(pac_name):
-            if cache[pac_name].is_installed:
-                print(pac_name + " already installed.")
-            else:
-                error_print(run_command("apt-get install {0}".format(pac_name)))
-        else:
-            print("failed to install package " + pac_name)
-
+# ======= Stdout/Prompt Functions =========
 
 def prompt(question, default="yes"):
     global force
@@ -80,7 +64,7 @@ def prompt(question, default="yes"):
     The "answer" return value is True for "yes" or False for "no".
     """
     valid = {"yes": True, "y": True, "ye": True,
-             "no": False, "n": False}
+            "no": False, "n": False}
     if force:
         return valid[default]
 
@@ -102,22 +86,12 @@ def prompt(question, default="yes"):
             return valid[choice]
         else:
             sys.stdout.write("Please respond with 'yes' or 'no' "
-                             "(or 'y' or 'n').\n")
-           
-def abspath(path):
-    return os.path.abspath(os.path.expanduser(path))
+                    "(or 'y' or 'n').\n")
 
 
-def error_print(val):
-    if val:
-        print("Failed. Error Code " + str(val))
-    else:
-        print("Success")
 
-    return val
-
-def ask_user(prompt):
-    valid = {"yes":True, 'y':True, '':True, "no":False, 'n':False}
+            def ask_user(prompt):
+                valid = {"yes":True, 'y':True, '':True, "no":False, 'n':False}
     while True:
         print(prompt)
         choice = input().lower()
@@ -126,19 +100,13 @@ def ask_user(prompt):
         else:
             print("Enter a correct choice.", file=stderr)
 
-def directories(**kwargs):
-    profile = kwargs.get('profile')
-    dirlist = profile.get('directories')
-    [create_directory(path) for path in dirlist]
+def error_print(val):
+    if val:
+        print("Failed. Error Code " + str(val))
+    else:
+        print("Success")
 
-
-def create_directory(path):
-
-    exp = os.path.expanduser(path)
-    if (not os.path.isdir(exp)):
-        print("generating directory {0}".format(exp))
-        os.makedirs(exp)
-
+    return val
 
 def fail(msg, src, dest):
     print("[FAILED] " + msg + " src: {0} dest: {1}".format(src,dest))
@@ -152,18 +120,108 @@ def success(msg, src, dest):
     print("Success  "  + msg + " src: {0} dest : {1}".format(src,dest))
     return True
 
+def notify_config(msg):
+    print("[Configuring " + msg+']')
 
-def add_cfg(cfg):
-    src = abspath(cfg.get("src"))
-    dest = abspath(cfg.get("dest"))
-    if assert_path(src,dest):
-        with open(src, 'r') as a, open(dest, 'a+') as b:
-            for line in a:
-                b.write(line)
+def notify(status):
+    print("["+status+"]")
+
+def notify_msg(status, msg):
+    print("["+status+"] " + msg )
+
+# ======= System path Functions =========
+
+def abspath(path):
+    return os.path.abspath(os.path.expanduser(path))
+
+def set_working_dir(path):
+    os.chdir(os.path.expanduser(os.path.abspath(os.path.dirname(path))))
+
+def exists(path):
+    return os.path.exists(abspath(path))
+
+def dirname(path):
+    return os.path.dirname(abspath(path))
+
+def assure_parent(path):
+    folder = dirname(abspath(path))
+    if not exists(folder):
+        print("making {}".format(folder))
+        os.makedirs(folder)
 
 
-def prof(kwargs):
-    return 
+
+
+
+
+
+
+
+# ============= Utilitary Functions  ===================
+
+def backup_file(prepath, backupdest):
+    notify_msg('backing up', prepath)
+    postpath = os.path.relpath(abspath(prepath), os.path.expanduser('~'))
+    print(backupdest)
+    backupdest = os.path.join(abspath(backupdest), postpath).replace("..", "xx")
+    parent_folder = os.path.dirname(backupdest)
+    if not os.path.exists(parent_folder):
+        os.makedirs(parent_folder)
+    if os.path.isfile(prepath):
+        shutil.copy(prepath, backupdest)
+    else:
+        shutil.copytree(prepath, backupdest)
+    print("backup done!")
+
+def copy_path(src, dest, force=False, announce=False):
+    dest = os.path.expanduser(dest)
+    src = os.path.abspath(src)
+    if os.path.exists(dest):
+        if ask_user(dest+ " exists, replace it? [Y/n]"):
+            if os.path.isfile(dest):
+                os.remove(dest)
+            else:
+                shutil.rmtree(dest)
+        else:
+            return
+    if announce:
+        print("copying {0} -> {1}".format(src,dest))
+    assure_parent(dest)
+
+    if os.path.isfile(src):
+        shutil.copy(src, dest)
+    else:
+        shutil.copytree(src, dest)
+
+
+
+
+
+# ======================================================
+#                       FEATURES
+# ======================================================
+# To add a feature, simply 'def featurename(**kwargs):' 
+# and code ahead. the profile is in kwargs.get('profile')
+# and options such as force is in kwargs.get('options')
+# make sure to add the featurename in the list under
+# load_features
+
+
+# ===================[Directories]===================
+
+def directories(**kwargs):
+    profile = kwargs.get('profile')
+    dirlist = profile.get('directories')
+    [create_directory(path) for path in dirlist]
+
+def create_directory(path):
+    exp = os.path.expanduser(path)
+    if (not os.path.isdir(exp)):
+        print("generating directory {0}".format(exp))
+        os.makedirs(exp)
+
+
+# ===================[Link]==========================
 
 def link(**kwargs):
     profile = kwargs.get('profile')
@@ -171,8 +229,7 @@ def link(**kwargs):
     backup = profile.get('backup')
     links = profile.get('link' )
     for src, dest in links.items():
-        create_symlink(src, links[src], options.replace, backupdest=backup)
-
+        create_symlink(src, links[src], options.get('replace'), backupdest=backup)
 
 def create_symlink(src, dest, replace, backupdest):
     reldest = dest
@@ -202,51 +259,7 @@ def create_symlink(src, dest, replace, backupdest):
     assure_parent(dest)
     os.symlink(src, dest)
 
-def exists(path):
-    return os.path.exists(abspath(path))
-
-def dirname(path):
-    return os.path.dirname(abspath(path))
-
-def assure_parent(path):
-    folder = dirname(abspath(path))
-    if not exists(folder):
-        print("making {}".format(folder))
-        os.makedirs(folder)
-
-def backup_file(prepath, backupdest):
-    notify_msg('backing up', prepath)
-    postpath = os.path.relpath(abspath(prepath), os.path.expanduser('~'))
-    backupdest = os.path.join(abspath(backupdest), postpath).replace("..", "xx")
-    parent_folder = os.path.dirname(backupdest)
-    if not os.path.exists(parent_folder):
-        os.makedirs(parent_folder)
-    if os.path.isfile(prepath):
-        shutil.copy(prepath, backupdest)
-    else:
-        shutil.copytree(prepath, backupdest)
-    print("backup done!")
-
-
-def copy_path(src, dest, force=False, announce=False):
-    dest = os.path.expanduser(dest)
-    src = os.path.abspath(src)
-    if os.path.exists(dest):
-        if ask_user(dest+ " exists, replace it? [Y/n]"):
-            if os.path.isfile(dest):
-                os.remove(dest)
-            else:
-                shutil.rmtree(dest)
-        else:
-            return
-    if announce:
-        print("copying {0} -> {1}".format(src,dest))
-    assure_parent(dest)
-    
-    if os.path.isfile(src):
-        shutil.copy(src, dest)
-    else:
-        shutil.copytree(src, dest)
+# ===================[Backup]========================
 
 def backup(**kwargs):
     profile = kwargs['profile']
@@ -259,6 +272,26 @@ def backup(**kwargs):
     else:
         os.makedirs(dest)
 
+# ===================[Install]========================
+
+def install(**kwargs):
+
+    cache = apt.Cache()
+    js = kwargs['profile']
+    dependencies  = js.get("install")
+
+    for pac_name in dependencies:
+        #cache = apt.Cache()
+        if cache.has_key(pac_name):
+            if cache[pac_name].is_installed:
+                print(pac_name + " already installed.")
+            else:
+                error_print(run_command("apt-get install {0}".format(pac_name)))
+        else:
+            print("failed to install package " + pac_name)
+
+# ===================[Commands]========================
+
 def commands(**kwargs):
     profile = kwargs.get('profile')
     options = kwargs.get('options')
@@ -267,39 +300,50 @@ def commands(**kwargs):
         run_command(command)
 
 def run_command(command):
-    return os.system(command) #for new local branch for new local branch
+    return os.system(command)
 
-def notify_config(msg):
-    print("[Configuring " + msg+']')
+# ===================[Add your own]====================
+# ===================[Add your own]====================
+# ===================[Add your own]====================
 
-def notify(status):
-    print("["+status+"]")
-
-def notify_msg(status, msg):
-    print("["+status+"] " + msg )
+# ============= Fundamentals ==========================
 
 def load_features(argvars):
+    options = argvars.__dict__
     cfgflg=['directories',
             'install',
             'link',
             'commands',
             'backup']
-    flags = {k: argvars[k] for k in cfgflg if k in argvars}
-    return flags
+    flags = {k: options[k] for k in cfgflg if k in options}
+    return flags, options
 
-def set_working_dir(path):
-    os.chdir(os.path.expanduser(os.path.abspath(os.path.dirname(path))))
+def run_features(enabled_features, profile, options):
+    error_str = "[ERROR]"
+    kwa = {'profile':profile, 'options': options}
+
+    for f in enabled_features:
+        try:
+            assert_feature(profile.get(f.__name__), f.__name__)
+            f(**kwa)
+        except ImportWarning:
+            print(error_str+ " Missing [" +f.__name__+ "] field from json profile")
+        except Exception as default:
+            print(error_str+ " Failed to call feature [" + f.__name__ + "].")
+            print(default)
+
+
 
 def main():
     # Command line options
     # Parser details
     parser = argparse.ArgumentParser(description="""
     The script will not run any feature unless a flag is supplied. Each included flag enables the corresponding feature. Run with '-a' to include all.
-    
+
     """)
     parser.add_argument("config", help="the JSON file you want to use")
     parser.add_argument("-r", "--replace", action="store_true",
-                        help="replace files/folders if they already exist")
+            help="replace files/folders if they already exist")
     parser.add_argument("-f", "--force", action="store_true", default=False,
             help="omits [Y/N] prompts and chooses the default value for the choice")
     parser.add_argument("-a", "--all", action="store_true", default=False,
@@ -313,40 +357,24 @@ def main():
     parser.add_argument("-c", "--commands", action="store_true", default=False,
             help="activates command procedure")
 
-    #Loading data
+    # Loading data
     notify("loading data")
     args = parser.parse_args()
-    js = json.load(open(args.config))
-
+    profile = json.load(open(args.config))
 
     # Set woring directory to folder with config file
     set_working_dir(args.config)
-    options = args.__dict__
 
-    print(options)
-    module_list = load_features(options)
+    # Collect features and options
+    feature_list, options = load_features(args)
 
     # Handle optional flags
-    if options['all']: module_list = {x: True for x in module_list}    
-    print(module_list)
-    kwa = {'profile':js, 'options': options}
-     
-    module_functions = [globals()[x] for x in module_list if module_list[x] == True]
-    print(module_functions)
-    error_str = "[ERROR]"
-    sys.exit()
-    for f in features:
-        if module_list.get(f.__name__):
-            try:
-                assert_feature(js.get(f.__name__), f.__name__)
-                f(**kwa)
-            except ImportWarning:
-                print(error_str+ " Missing [" +f.__name__+ "] field from json profile")
-            except Exception as default:
-                print(error_str+ " Failed to call feature [" + f.__name__ + "].")
-                print(default)
+    if options['all']: feature_list = {x: True for x in feature_list}    
 
+    # Enabled_features is a list of function-pointers
+    enabled_features = [globals()[x] for x in feature_list if feature_list[x] == True]
 
+    run_features(enabled_features, profile, options)
     print("Done!")
 
 
